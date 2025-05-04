12bf1cbfdce114d7ff07761aa246390762ad9717
t.z <t.z@t.z>
2025-05-04 13:02:14 +0000

    feat: Tailwind CSSの導入とフォームのスタイル調整

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // 静的ファイルの提供
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   // EJSテンプレートエンジンの設定
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
